8d502ec7bf095f43d843ff845b349697f9afd407
Alex <dok.go@yandex.ru>
2026-04-17 12:41:03 +0500

    'test'

--- src/assets/js/splide.js
+++ src/assets/js/splide.js
@@ -4,7 +4,19 @@ import Splide from '@splidejs/splide';
 
 document.addEventListener( 'DOMContentLoaded', function() {
     window.slide1 = new Splide('.portfolio__slider', {
+        type   : 'loop',
+        pagination: false,
+        perPage: 3,
+        gap: 20,
 
+        breakpoints: {
+            576: {
+                perPage: 1,
+            },
+            1024: {
+                perPage: 2,
+            },
+        }
     });
     window.slide1.mount();
 });
